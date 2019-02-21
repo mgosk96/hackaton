fc2fe0816e8b6c33a6132b04ffd4e4d6f5be1346
mgosk96 <m.gosk96@gmail.com>
2019-02-21 14:27:42 +0100

    nieudana choinka

--- zad 2/choinka.js
+++ zad 2/choinka.js
@@ -1,1 +1,34 @@
 console.log("choinka.js");
+const poziomy = document.getElementById('poziomy');
+const pojemnosc = document.getElementById('pojemnosc');
+const btn = document.querySelector('button');
+const result = document.getElementById('result');
+let treeArr;
+let iloscWierszy=5;
+let iloscKolumn=5;
+let napis = '';
+
+btn.addEventListener('click', () => {
+    result.textContent="";
+    let pietra = Math.floor(poziomy.value / pojemnosc.value);
+    result.textContent += pietra;
+})
+
+
+    var n = 8;
+ 
+    for(i=1; i<=n; i++)
+    {   //Prints trailing spaces
+        for(j=i; j<n; j++)
+        {
+            document.write(" ");
+        }
+ 
+        //Prints the pyramid pattern
+        for(j=1; j<=(2*i-1); j++)
+        {
+            document.write("*");
+        }
+ 
+        document.write("<br>");
+        }
---
+++ zad3/mock_tree.js
@@ -0,0 +1,44 @@
+const tree = [
+    {
+        label: "A",
+        value: "A",
+        children: [
+            {
+                label: "A1",
+                value: "A1",
+                children: []
+            },
+            {
+                label: "A2",
+                value: "A2",
+                children: []
+            },
+            {
+                label: "A3",
+                value: "A3",
+                children: []
+            }
+        ]
+    },
+    {
+        label: "B",
+        value: "B",
+        children: [
+            {
+                label: "B1",
+                value: "B1",
+                children: []
+            },
+            {
+                label: "B2",
+                value: "B2",
+                children: []
+            },
+            {
+                label: "B3",
+                value: "B3",
+                children: []
+            }
+        ]
+    }
+]
