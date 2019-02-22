c3b57c67f695e2848cb0b8c2dc031262a876618a
mgosk96 <m.gosk96@gmail.com>
2019-02-22 12:07:59 +0100

    zad 3 without searchbox

--- zad3/tree/src/app/tree/tree.component.ts
+++ zad3/tree/src/app/tree/tree.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {tree} from '../../assets/mock_tree';
+import { element } from '@angular/core/src/render3';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+
 
 @Component({
   selector: 'app-tree',
@@ -8,32 +11,56 @@ import {tree} from '../../assets/mock_tree';
 })
 export class TreeComponent implements OnInit {
   // @ViewChild('')
-  index;
   item;
-  children;
-  selected = false;
   selection = [];
-  itemChecked = false;
+  disp = false;
   constructor() { }
 
   ngOnInit() {
     this.item = tree.map((a) => a);
     console.log(this.item);
-
-    this.children = this.item.map(a => a.children);
-    console.log(this.children);
   }
 
   check(isSelected, item) {
-  if (isSelected) {
-    this.selection.push(item.value);
-    console.log(this.selection);
-  } else {
-  this.index = this.selection.indexOf(item.value);
-   this.selection.splice(this.index, 1);
-   console.log(this.selection);
-  }
+    item.checked = isSelected;
+    if (item.checked === true) {
+      for (let i = 0; i < item.children.length ; i++) {
+        item.children[i].checked = isSelected;
+      }
+    } else if (item.checked === false) {
+      for ( let i = 0; i < item.children.length; i++ ) {
+        item.children[i].checked = false;
+      }
+    }
   }
 
+  display(item) {
+    if (item.checked) {
+      this.selection.push(item.value);
+      for (let i = 0; i < item.children.length; i++) {
+        if (item.children[i].checked) {
+           console.log(item.children[i]);
+           this.selection.push(item.children[i].value);
+          }
+        }
+      } else if (item.checked === false) {
+        this.selection.splice(this.selection.indexOf(item.value), 1);
+        console.log(this.selection);
+        for (let j = 0; j < item.children.length; j++ ) {
+          this.selection.splice(this.selection.indexOf(item.children[j].value, 1));
+        }
+      }
+    }
 
-}
+
+    toggle(parent) {
+      parent.showChildren = this.disp;
+      if (!parent.showChildren) {
+        this.disp = true;
+        parent.showChildren = this.disp;
+      } else {
+        this.disp = false;
+        parent.showChildren = this.disp;
+      }
+    }
+  }
